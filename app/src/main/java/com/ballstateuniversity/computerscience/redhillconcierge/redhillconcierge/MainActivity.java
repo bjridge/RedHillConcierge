517efb9a62119e9640226225517dc08229d08a4b
bjridge <bjridge@bsu.edu>
2017-01-26 17:08:59 -0500

    Add Test TextView for output

--- app/src/main/java/com/ballstateuniversity/computerscience/redhillconcierge/redhillconcierge/MainActivity.java
+++ app/src/main/java/com/ballstateuniversity/computerscience/redhillconcierge/redhillconcierge/MainActivity.java
@@ -4,21 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.database.*;//FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //connect to database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference();
+        TextView editText = (TextView) findViewById(R.id.testOutput);
+        editText.setText("did it work?");
 
 
     }
+
+
 }
