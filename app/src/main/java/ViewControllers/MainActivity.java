cf29cd6aaf7e86c9bd15c9dce12d87a16d1997e1
bjridge <bjridge@bsu.edu>
2017-03-17 15:15:19 -0400

    Created horse_layout feed

--- app/src/main/java/ViewControllers/MainActivity.java
+++ app/src/main/java/ViewControllers/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        setContentView(R.layout.horse_layout);
         //initializeEditTexts();
         //initializeButton();
         //controller = new DatabaseController();
