d5c31e6dc9456223f4e369f6170124c76455333c
bjridge <bjridge@bsu.edu>
2017-03-18 23:28:21 -0400

    New User Interface Branch

---
+++ app/src/main/java/ViewControllers/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package ViewControllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.ballstateuniversity.computerscience.redhillconcierge.redhillconcierge.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
