2d14681bff78f12ae0844706ce46888fd1731b02
bjridge <bjridge@bsu.edu>
2017-03-28 17:03:47 -0400

    setup horse list view objects - draft 1

--- app/src/main/java/Activities/Fragments/MyHorsesTab.java
+++ app/src/main/java/Activities/Fragments/MyHorsesTab.java
@@ -5,13 +5,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.ballstateuniversity.computerscience.redhillconcierge.redhillconcierge.R;
 
 import DataControllers.Contact;
+import DataControllers.Horse;
+import DataControllers.HorseAdapter;
 import DataControllers.User;
 
 public class MyHorsesTab extends MyFragment {
+
+    ListView myHorsesList;
+    ListView sharedHorsesList;
+
+
+
+
 
     public MyHorsesTab() {
         // Required empty public constructor
@@ -27,6 +39,30 @@ public class MyHorsesTab extends MyFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.tab__my_horses, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        myHorsesList = (ListView) getView().findViewById(R.id.my_horses_list);
+        sharedHorsesList = (ListView) getView().findViewById(R.id.shared_horses_list);
+
+        Horse testHorse = new Horse();
+        testHorse.setKey("1");
+        testHorse.setName("Test Horse");
+        testHorse.setBreed("test breed");
+        testHorse.setColor("test color");
+        testHorse.setSex("test breed");
+        testHorse.setStallNumber(4);
+
+        Horse[] testHorses = new Horse[]{testHorse, testHorse, testHorse};
+
+        HorseAdapter horseArrayAdapter = new HorseAdapter(getContext(),R.layout.custom_horse_list_item, testHorses );
+
+        myHorsesList.setAdapter(horseArrayAdapter);
+
+
     }
 
 
