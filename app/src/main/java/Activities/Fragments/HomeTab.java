1df73b504e38efeca217dcecc11aba302ba831c7
bjridge <bjridge@bsu.edu>
2017-04-26 13:08:51 -0400

    Home page done

--- app/src/main/java/Activities/Fragments/HomeTab.java
+++ app/src/main/java/Activities/Fragments/HomeTab.java
@@ -18,6 +18,7 @@ public class HomeTab extends Fragment {
 
     TextView nameOutput;
     TextView typeOutput;
+    TextView statement;
 
     public HomeTab() {
 
@@ -41,9 +42,15 @@ public class HomeTab extends Fragment {
         application = (MyApplication)  getActivity().getApplication();
         nameOutput = (TextView) getView().findViewById(R.id.home_name);
         typeOutput = (TextView) getView().findViewById(R.id.home_user_type);
+        statement = (TextView) getView().findViewById(R.id.statement);
         Log.v("important", "about to get application");
         nameOutput.setText(application.getUser().getFirstName());
         typeOutput.setText(application.getUser().getType());
+        String statementString = "A great horse will change your life. The truly special ones define it.. \n\n We ride to fly, to feel, to breathe, to bond, to relax, to get away, to feel strong, to love, WE RIDE TO LIVE! \n\n There" +
+                "'" + "s nothing quite so special as the bond between a girl, her dog and her horse \n\n A barn is a sanctuary in an unsettled world, a sheltered place where life’s true priorities are clear.  When you take a step back, it" +
+                "’" + "s not just about horse" + "’" + "s – it" +
+                "’" +"s about love, life and learning \n\n Every day is a good day when you ride \n\n I work hard so my horse can have nicer stuff then me.";
+        statement.setText(statementString);
 
     }
 
